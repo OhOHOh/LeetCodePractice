e18b6edf074b4527281580e014d6525a58431d1f
Planck <yu15221589025@gmail.com>
2017-09-24 14:14:06 +0800

    第38题，使用StringBuilder来创建一个经常改变的String类型池

---
+++ No38.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+
+public class No38 {
+    public String countAndSay(int n) {
+        ArrayList<Integer> result = new ArrayList<>();
+        String s = "1";
+        for (int i = 1; i < n; i++) {
+            s = countIdx(s);
+        }
+        return s;
+    }
+
+    private String countIdx(String s) {
+        StringBuilder sb = new StringBuilder();
+        char c = s.charAt(0);
+        int count = 1;
+        for (int i = 1; i < s.length(); i++) {
+            if (s.charAt(i) == c) {
+                count++;
+            } else {
+                sb.append(count);
+                sb.append(c);
+                c = s.charAt(i);
+                count = 1;
+            }
+        }
+        sb.append(count);
+        sb.append(c);
+        return sb.toString();
+    }
+}
