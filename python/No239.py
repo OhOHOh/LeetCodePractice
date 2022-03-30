14e59bf90c35f407c0eeea9902ebc623eb85346c
Planck <15221589025@163.com>
2022-03-30 12:15:52 +0800

    COOL

---
+++ python/No239.py
@@ -0,0 +1,55 @@
+# -*- coding: UTF-8 -*-
+from collections import deque
+
+class MyQueue1:
+    def __init__(self):
+        self.queue = list()
+    def m_push(self, num):
+        while self.queue and self.queue[-1] < num:
+            self.queue.pop()
+        #while self.queue and self.queue[0] <= num:
+        #    self.queue.pop(0)
+        self.queue.append(num)
+
+    def m_pop(self, num):
+        if self.queue[0] == num:
+            return self.queue.pop(0)
+        
+    def m_front(self):
+        return self.queue[0]
+
+class MyQueue:
+    def __init__(self):
+        self.queue = deque()
+    def m_push(self, num):
+        while self.queue and self.queue[-1] < num:
+            self.queue.pop()
+        #while self.queue and self.queue[0] <= num:
+        #    self.queue.pop(0)
+        self.queue.append(num)
+
+    def m_pop(self, num):
+        if self.queue[0] == num:
+            return self.queue.popleft()
+        
+    def m_front(self):
+        return self.queue[0]
+
+class Solution:
+    def maxSlidingWindow(self, nums, k):
+        rtn = []
+        queue = MyQueue()
+        for i in range(k):
+            queue.m_push(nums[i])
+        rtn.append(queue.m_front())
+            
+        for i in range(k, len(nums)):
+            queue.m_pop(nums[i-k])
+            queue.m_push(nums[i])
+            rtn.append(queue.m_front())
+        return rtn
+
+s = Solution()
+#print(s.maxSlidingWindow([1,3,-1,-3,5,3,6,7], 3))
+#print(s.maxSlidingWindow([1,3,1,2,0,5], 3))
+print(s.maxSlidingWindow([-7,-8,7,5,7,1,6,0], 4))
